2f910e0de9f964e4502d7d143cbbc3b95292f0d8
Gaurav Sikka01 <Gaurav.Sikka01@ad.infosys.com>
2020-06-24 00:29:32 +0530

    fix

--- webpack.client.js
+++ webpack.client.js
@@ -1,48 +1,56 @@
-"use strict";
-const path = require("path");
-const webpack = require("webpack");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const packageJson = require("./package.json");
+'use strict'
+const path = require('path')
+const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const packageJson = require('./package.json')
 
 const plugins = [
   new MiniCssExtractPlugin({
     // Options similar to the same options in webpackOptions.output
     // both options are optional
-    filename: "style.css",
+    filename: 'style.css'
   }),
 
   // define global vars
-  new webpack.DefinePlugin({
-    __CLIENT__: true,
-    __APPVERSION__: JSON.stringify(packageJson.version),
-  }),
+  new webpack.DefinePlugin(
+    {
+      __CLIENT__: true,
+      __APPVERSION__: JSON.stringify(packageJson.version)
+    }
+  ),
   new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-  new webpack.optimize.AggressiveMergingPlugin(),
-];
+  new webpack.optimize.AggressiveMergingPlugin()
+]
 
 const optimization = {
   splitChunks: {
     cacheGroups: {
       vendor: {
-        chunks: "initial",
-        name: "vendor",
-        test: "vendor",
-      },
-    },
-  },
-};
+        chunks: 'initial',
+        name: 'vendor',
+        test: 'vendor'
+      }
+    }
+  }
+}
 
 module.exports = {
   // devtool: 'source-map',
   // create a separte bundle for the modules which do not change very often
   // so that is bundle is cached by the browser
   entry: {
-    app: "./src/client/index",
-    vendor: ["react", "redux", "react-dom", "react-router", "react-intl"],
+    app: './src/client/index',
+    vendor: [
+      'react',
+      'redux',
+      'react-dom',
+      'react-router',
+      'react-intl'
+    ]
   },
   output: {
-    path: path.join(__dirname, "/dist"),
-    filename: "[name].bundle.js",
+    path: path.join(__dirname, '/dist'),
+    filename: '[name].bundle.js'
   },
   module: {
     rules: [
@@ -50,8 +58,8 @@ module.exports = {
         test: /\.(jsx|js)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
-        },
+          loader: 'babel-loader'
+        }
       },
 
       // load styles
@@ -59,50 +67,40 @@ module.exports = {
         test: /\.less/,
         use: [
           MiniCssExtractPlugin.loader,
-          "css-loader",
+          'css-loader',
           {
-            loader: "postcss-loader",
+            loader: 'postcss-loader'
           },
-          "less-loader",
-        ],
+          'less-loader'
+        ]
       },
 
       {
         test: /\.(scss|css)$/,
         use: [
           MiniCssExtractPlugin.loader,
-          "css-loader",
+          'css-loader',
           {
-            loader: "postcss-loader",
+            loader: 'postcss-loader'
           },
-          "sass-loader",
-        ],
+          'sass-loader']
       },
 
       // Load images
-      { test: /\.jpg/, loader: "url-loader?limit=10000&mimetype=image/jpg" },
-      { test: /\.gif/, loader: "url-loader?limit=10000&mimetype=image/gif" },
-      { test: /\.png/, loader: "url-loader?limit=10000&mimetype=image/png" },
-      { test: /\.svg/, loader: "url-loader?limit=10000&mimetype=image/svg" },
+      { test: /\.jpg/, loader: 'url-loader?limit=10000&mimetype=image/jpg' },
+      { test: /\.gif/, loader: 'url-loader?limit=10000&mimetype=image/gif' },
+      { test: /\.png/, loader: 'url-loader?limit=10000&mimetype=image/png' },
+      { test: /\.svg/, loader: 'url-loader?limit=10000&mimetype=image/svg' },
 
       // Load fonts
-      {
-        test: /\.woff(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: "url-loader?limit=10000&mimetype=application/font-woff",
-      },
-      {
-        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: "file-loader",
-      },
-      {
-        test: /\.woff(2)?(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "url-loader?limit=10000&mimetype=application/font-woff",
-      },
-    ],
+      { test: /\.woff(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff' },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader' },
+      { test: /\.woff(2)?(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff' }
+    ]
   },
   plugins: plugins,
   optimization: optimization,
   resolve: {
-    extensions: [".js", ".jsx", ".less", ".css", ".scss"],
-  },
-};
+    extensions: ['.js', '.jsx', '.less', '.css', '.scss']
+  }
+}
